c05ee6ed33f1cb9d676b10cc338879d8a58f8c30
Muhammad Fariz Rahman <muhammad.fariz@kanosolution.com>
2022-07-19 20:35:33 +0700

    update endpoint

--- app/confirmation/router.js
+++ app/confirmation/router.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const {confirmList, actionCreate, countConfirmation} = require('./controller');
+const {
+  confirmList,
+  actionCreate,
+  countConfirmation,
+  getConfirmByName,
+} = require('./controller');
 
 router.post('/list', confirmList);
 router.post('/create', actionCreate);
 router.post('/count', countConfirmation);
+router.post('/by-name', getConfirmByName);
 
 module.exports = router;
