3068e9aea721f06f5995356b467f5ed889d77a4a
Muhammad Fariz Rahman <muhammad.fariz@kanosolution.com>
2022-07-21 15:05:41 +0700

    setup meta tags

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/responsive.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type {AppProps} from 'next/app';
 import Head from 'next/head';
+import {useRouter} from 'next/router';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-activity/dist/library.css';
@@ -12,14 +13,47 @@ import 'moment/locale/id';
 moment.locale('id');
 
 function MyApp({Component, pageProps}: AppProps) {
+  const router = useRouter();
+  const {place} = router.query;
+
+  const coupleName = place === 'kdr' ? 'Fariz & Rika' : 'Rika & Fariz';
+  const imgUrl = '/img/web-thumb.jpg';
+
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <title>The Wedding - Rika & Fariz</title>
-        <meta property="og:title" content="The Wedding - Rika & Fariz" />
+        <title>The Wedding - {coupleName}</title>
+
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={`The Wedding - ${coupleName}`} />
+        <meta
+          property="og:description"
+          content="We invited you to celebrate our wedding"
+        />
+        <meta
+          property="og:url"
+          content="https://rikafariz-wedding.vercel.app"
+        />
+        <meta name="keywords" content="rikafariz-wedding, ayca-wedding" />
+        <meta name="author" content="Muhammad Fariz Rahman" />
+        <meta property="og:site_name" content="Wedding Invitation" />
+        <meta property="article:section" content="Website Invitation" />
+        <meta property="og:image" content={imgUrl} />
+        <meta property="og:image:secure_url" content={imgUrl} />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="400" />
+        <meta property="og:image:alt" content={`The Wedding - ${coupleName}`} />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={`The Wedding - ${coupleName}`} />
+        <meta
+          name="twitter:description"
+          content="We invited you to celebrate our wedding"
+        />
+        <meta name="twitter:image" content={imgUrl} />
       </Head>
       <Component {...pageProps} />
       <script
